a2f0e206e83e9543c9537c2c6210025d53a3d299
zzwarnick22 <121060038+zzwarnick22@users.noreply.github.com>
2022-12-20 17:22:05 -0500

    Add files via upload
    A program I wrote to understand boolean operators in C++

--- main.cpp
+++ main.cpp
@@ -1,36 +1,31 @@
 #include <iostream>
-#include <cstdlib>
-#include <cstdio>
 
 using namespace std;
 
 int main(int nNumberofArgs, char*pszArgs[])
 {
+    cout.setf(cout.boolalpha);
 
-    //enter the temperature in Celsius
-    int celsius;
-    cout<<"Enter the temperature in Celsius: ";
-    cin>>celsius;
+    int nArg1;
+    cout << "Input Value 1: ";
+    cin >> nArg1;
 
-    int factor;
-    factor = 212-32;
+    int nArg2;
+    cout << "Input Value 2: ";
+    cin >> nArg2;
 
-    //calculate conversion factor for Celsius
-    //into Fahrenheit values
-    int fahrenheit;
-    fahrenheit = factor * celsius/100 +32;
+    bool b;
+    b = nArg1 == nArg2;
 
-    //output the results (followed by a NewLine)
-    cout<< "Fahrenheit value is: ";
-    cout<< fahrenheit << endl;
+    cout << "The statement, " << nArg1
+         << " equals "        << nArg2
+         << " is "            << b
+         << endl;
 
-
-    //wait until the user is ready before terminating the program
-    //to allow the user to see the program results
-
-    cout<< "Press Enter to continue... " << endl;
+    cout <<"Press Enter to continue... "  << endl;
     cin.ignore(10, '\n');
     cin.get();
     return 0;
+
 }
 
