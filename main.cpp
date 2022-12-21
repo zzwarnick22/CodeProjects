01e7ad7d7a0cb944fefa4e00b7edff77d0104ffa
zzwarnick22 <121060038+zzwarnick22@users.noreply.github.com>
2022-12-21 13:25:10 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,19 +6,30 @@ using namespace std;
 
 int main(int nNumberofArgs, char*pszArgs[])
 {
-    int nLoopCount;
-    cout << "Enter loop count: ";
-    cin >> nLoopCount;
 
-    while (nLoopCount > 0)
-    {
-        nLoopCount = nLoopCount - 1;
-        cout << "Only " << nLoopCount;
-        cout << " Loops to go " << endl;
-    }
-    cout<<"Press Enter to continue..." << endl;
+    //enter the temperature in Celsius
+    int celsius;
+    cout<<"Enter the temperature in Celsius: ";
+    cin>>celsius;
+
+    int factor;
+    factor = 212-32;
+
+    //calculate conversion factor for Celsius
+    //into Fahrenheit values
+    int fahrenheit;
+    fahrenheit = factor * celsius/100 +32;
+
+    //output the results (followed by a NewLine)
+    cout<< "Fahrenheit value is: ";
+    cout<< fahrenheit << endl;
+
+
+    //wait until the user is ready before terminating the program
+    //to allow the user to see the program results
+    cout<< "Press Enter to continue... " << endl;
     cin.ignore(10, '\n');
     cin.get();
-
     return 0;
 }
+
