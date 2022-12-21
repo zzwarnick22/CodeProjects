7b77c595317fdee21dd744c5c53c25e06d976ffb
zzwarnick22 <121060038+zzwarnick22@users.noreply.github.com>
2022-12-21 08:58:39 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,24 @@
 #include <iostream>
+#include <cstdlib>
+#include <cstdio>
 
 using namespace std;
 
 int main(int nNumberofArgs, char*pszArgs[])
 {
-    cout.setf(cout.boolalpha);
+    int nLoopCount;
+    cout << "Enter loop count: ";
+    cin >> nLoopCount;
 
-    int nArg1;
-    cout << "Input Value 1: ";
-    cin >> nArg1;
-
-    int nArg2;
-    cout << "Input Value 2: ";
-    cin >> nArg2;
-
-    bool b;
-    b = nArg1 == nArg2;
-
-    cout << "The statement, " << nArg1
-         << " equals "        << nArg2
-         << " is "            << b
-         << endl;
-
-    cout <<"Press Enter to continue... "  << endl;
+    while (nLoopCount > 0)
+    {
+        nLoopCount = nLoopCount - 1;
+        cout << "Only " << nLoopCount;
+        cout << " Loops to go " << endl;
+    }
+    cout<<"Press Enter to continue..." << endl;
     cin.ignore(10, '\n');
     cin.get();
+
     return 0;
-
 }
-
